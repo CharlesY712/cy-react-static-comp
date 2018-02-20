32edda909fbd9ce613cbc22e1f33269f7c5677ce
Charles Yach <CharlesY712@gmail.com>
2018-02-19 23:00:08 -0700

    Updating and resolving conflicts

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,18 @@ import React from 'react';
 import './Footer.css';
 
 const Footer = () => {
-  // WHAT GOES HERE?
+  return (
+    <div className="Footer">
+      <a href="">Getting started</a>
+      <a href="">Help</a>
+      <a href="">Status</a>
+      <a href="">Blog</a>
+      <a href="">Careers</a>
+      <a href="">Privacy</a>
+      <a href="">Terms</a>
+      <a href="">About</a>
+    </div>
+  )
 }
 
 export default Footer;
